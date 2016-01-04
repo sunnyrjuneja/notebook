c54948daa08649e64737c18d6fe44c52269116b7
Sunny Juneja <jrsunny@gmail.com>
2016-01-03 21:30:54 -0800

    Finish reading upto page 36.

---
+++ understanding_computation/chap_2.md
@@ -0,0 +1,12 @@
+* formal semantics
+* abstract syntax tree
+* abstract machine
+* operational semantics - meaning of a programming language by defining rules for how its porgrams execute on some kind of device (abstrac t machine)
+* small step semantics
+* value, expression
+* metalanguage
+* inference rules, reduction relation
+* formal verification
+* computer assisted proof
+* statements
+* pure / impure
---
+++ understanding_computation/simple.rb
@@ -0,0 +1,179 @@
+class Number < Struct.new(:value)
+  def reducible?
+    false
+  end
+
+  def to_s
+    value.to_s
+  end
+
+  def inspect
+    "<<#{self}>>"
+  end
+end
+
+class Add < Struct.new(:left, :right)
+  def reduce(environment)
+    if left.reducible?
+      Add.new(left.reduce(environment), right)
+    elsif right.reducible?
+      Add.new(left, right.reduce(environment))
+    else
+      Number.new(left.value + right.value)
+    end
+  end
+
+  def reducible?
+    true
+  end
+
+  def to_s
+    "#{left} + #{right}"
+  end
+
+  def inspect
+    "<<#{self}>>"
+  end
+end
+
+class Multiply < Struct.new(:left, :right)
+  def reduce(environment)
+    if left.reducible?
+      Multiply.new(left.reduce(environment), right)
+    elsif right.reducible?
+      Multiply.new(left, right.reduce(environment))
+    else
+      Number.new(left.value * right.value)
+    end
+  end
+
+  def reducible?
+    true
+  end
+
+  def to_s
+    "#{left} * #{right}"
+  end
+
+  def inspect
+    "<<#{self}>>"
+  end
+end
+
+class Boolean < Struct.new(:value)
+  def reducible?
+    false
+  end
+
+  def to_s
+    value.to_s
+  end
+
+  def inspect
+    "<<#{self}>>"
+  end
+end
+
+class LessThan < Struct.new(:left, :right)
+  def reducible?
+    true
+  end
+
+  def reduce(environment)
+    if left.reducible?
+      LessThan.new(left.reduce(environment), right)
+    elsif right.reducible?
+      LessThan.new(left, right.reduce(environment))
+    else
+      Boolean.new(left.value < right.value)
+    end
+  end
+      
+  def to_s
+    "#{left} < #{right}"
+  end
+
+  def inspect
+    "<<#{self}>>"
+  end
+end
+
+class Variable < Struct.new(:name)
+  def reducible?
+    true
+  end
+
+  def reduce(environment)
+    environment[name]
+  end
+
+  def to_s
+    name.to_s
+  end
+
+  def inspect
+    "<<#{self}>>"
+  end
+end
+
+class DoNothing
+  def ==(other_statement)
+    other_statement.instance_of?(DoNothing)
+  end
+
+  def reducible?
+    false
+  end
+
+  def to_s
+    'do-nothing'
+  end
+
+  def inspect
+    "<<#{self}>>"
+  end
+end
+
+class Assign < Struct.new(:name, :expression)
+  def reduce(environment)
+    if expression.reducible?
+      [Assign.new(name, expression.reduce(environment)), environment]
+    else
+      [DoNothing.new, environment.merge({name => expression})]
+    end
+  end
+
+  def reducible?
+    true
+  end
+
+  def to_s
+    "#{name} = #{expression}"
+  end
+
+  def inspect
+    "<<#{self}>>"
+  end
+end
+
+class Machine < Struct.new(:statement, :environment)
+  def step
+    self.statement, self.environment = statement.reduce(environment)
+  end
+
+  def run
+    while statement.reducible?
+      puts "#{statement}, #{environment}"
+      step
+    end
+
+    puts "#{statement}, #{environment}"
+  end
+end
+
+m = Machine.new(
+  Assign.new(:x, Add.new(Variable.new(:x), Number.new(1))),
+  { x: Number.new(2) }
+)
+
+m.run
